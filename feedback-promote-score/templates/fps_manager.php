630a0dbb6a037dc6a8b106d1552d532d808160f2
Protap Mondal <62872859+protappro@users.noreply.github.com>
2023-01-10 17:56:00 +0530

    Add files via upload

---
+++ feedback-promote-score/templates/fps_manager.php
@@ -0,0 +1,23 @@
+<!-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.21/datatables.min.css"/> 
+	<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.21/datatables.min.js"></script> -->
+
+	<style>
+		.export_nps_btn{
+			color: #fff !important;
+			border-color: #004577 !important;
+			background: #004577 !important;
+		}
+		.export_nps_btn:hover, .export_nps_btn:active,.export_nps_btn:focus{
+			color: #fff !important;
+			border-color: #004577 !important;
+			background: #004577 !important;
+		}
+	</style>
+	<div class="rc-container">
+		<a class="button export_nps_btn" href="/wp-admin/?export_net_promoter_score_data=1" target="_blank">Export Net Promoter Score</a>		
+	</div>
+	<?php 
+	// require_once plugin_dir_path(__FILE__) . "class-wp-nps-logs-table.php";
+	// $obj_wp_nps_logs = new Nps_logs_Table();
+	?>
+
